37f2a5519c541b6f6d6186982accdcd6dd56abb1
Thomas August Lemqvist <dr.bumlehund@live.dk>
2017-05-03 18:34:51 +0200

    merge conflict

--- Rflr/app/src/main/java/dk/sdu/mmmi/ap/g17/rflr/MainMenu.java
+++ Rflr/app/src/main/java/dk/sdu/mmmi/ap/g17/rflr/MainMenu.java
@@ -127,11 +127,12 @@ public class MainMenu extends AppCompatActivity {
         mSensorManager.unregisterListener(mShakeDetector);
         super.onPause();
     }
-    }
+
 
     //Switch activity using an intent
-    public void startIngame(View v){
+    public void startIngame(View v) {
         Intent myIntent = new Intent(MainMenu.this, InGameActivity.class);
         //myIntent.putExtra("key", value); //Optional for passing extra info
         MainMenu.this.startActivity(myIntent);
+    }
 }
